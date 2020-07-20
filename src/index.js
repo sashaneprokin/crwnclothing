7cb8de73e709357e34843a90e939a04705539980
sneprokin <neprokinalex94@gmail.com>
2020-07-20 13:58:49 +0300

    updating rootReducer w latest configs

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import store from './redux/store';
+import {store, persistor} from './redux/store';
 
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
@@ -11,7 +12,9 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')

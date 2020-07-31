7afc1c74a6058090e8e10a9e8b0f8cc09fb99a2d
sneprokin <neprokinalex94@gmail.com>
2020-07-31 12:27:47 +0300

    async events integration

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,20 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = collectionsMap => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+export const fetchCollectionsStart = collectionsMap => ({
+  type: ShopActionTypes.fetchCollectionsStart
 });
+
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart())
+
+    collectionRef.get().then(snapshot => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      //updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+  }
+}

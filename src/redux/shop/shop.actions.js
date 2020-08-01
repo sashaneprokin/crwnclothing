5bbbed27fe98facafdc7054f230c94ad36595785
sneprokin <neprokinalex94@gmail.com>
2020-08-01 20:15:20 +0300

    modified collections-page x collections-overview components w Container Pattern

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,20 +1,35 @@
 import ShopActionTypes from './shop.types';
 
-import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import {
+  firestore,
+  convertCollectionsSnapshotToMap
+} from '../../firebase/firebase.utils';
 
-export const fetchCollectionsStart = collectionsMap => ({
-  type: ShopActionTypes.fetchCollectionsStart
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+  payload: collectionsMap
+});
+
+export const fetchCollectionsFailure = errorMessage => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage
 });
 
 export const fetchCollectionsStartAsync = () => {
   return dispatch => {
     const collectionRef = firestore.collection('collections');
-    dispatch(fetchCollectionsStart())
+    dispatch(fetchCollectionsStart());
 
-    collectionRef.get().then(snapshot => {
-      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-      //updateCollections(collectionsMap);
-      this.setState({ loading: false });
-    });
-  }
-}
+    collectionRef
+      .get()
+      .then(snapshot => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
